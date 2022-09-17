96d9d1c7ebfe8588b5861e1ff988ddcfc640c057
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-09-17 22:23:46 +0200

    [Config] init config w/ dynaconf

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from typing import Literal, Optional
 import discord
 from discord.ext import commands
 
+from config import settings
 from src.pidioul import Pidioul
 
 bot = Pidioul(command_prefix='/', intents=discord.Intents.all())
 
 try:
-	bot.run('', root_logger=True)
+	bot.run(settings.discord.token, root_logger=True)
 finally:
 	print('EXITING GRACEFULLY')
